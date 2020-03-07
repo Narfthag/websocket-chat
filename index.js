9433045b5a6e37d320a8a43ff6f160e183339c0b
Narfthag <alexvazquez695@yahoo.com>
2020-03-07 16:45:32 -0600

    Leccion 3

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var socket = require('socket.io');
 
 // App setup
 var app = express();
@@ -9,3 +10,10 @@ var server = app.listen(port, () => {
 
 
 app.use(express.static('public'));
+
+
+// socket configuracion
+var io = socket(server);
+io.on('connection', (socket) => {
+  console.log(`made socket connection with id ${socket.id}`);
+});
---
+++ public/chat.js
@@ -0,0 +1,3 @@
+// Hacer conexion
+
+var socket = io.connect('http://localhost:4000'); // Cambiar de ip 
